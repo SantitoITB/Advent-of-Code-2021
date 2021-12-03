5b538cc82da38407aedda706ff494ab62b311fd8
Santito <Elnumero1234>
2021-12-03 12:50:00 +0100

    add day01 and day02

---
+++ src/day02.kt
@@ -0,0 +1,38 @@
+//  --- Day 2: Dive! ---
+
+data class Command(val name: String, val value: Int)
+fun main() {
+    fun part1(input: List<Command>): Int {
+        var depth = 0
+        var horizontal = 0
+        for (i in input){
+            when (i.name){
+                "forward" -> horizontal += i.value
+                "down" -> depth += i.value
+                "up" -> depth -= i.value
+            }
+        }
+        return depth*horizontal
+    }
+
+    fun part2(input: List<Command>): Int {
+        var depth = 0
+        var horizontal = 0
+        var aim = 0
+        for (i in input){
+            when (i.name){
+                "forward" -> {
+                    horizontal += i.value
+                    depth += i.value * aim
+                }
+                "down" -> aim += i.value
+                "up" -> aim -= i.value
+            }
+        }
+        return depth*horizontal
+    }
+
+    val input = readInput("day02").map { val (name, value) = it.split(" ");Command(name,value.toInt())}
+    println(part1(input))
+    println(part2(input))
+}
---
+++ src/day03.kt
@@ -0,0 +1,15 @@
+//  --- Day 3: Binary Diagnostic ---
+
+fun main() {
+    fun part1(input: List<String>): List<String> {
+        return input
+    }
+
+    fun part2(input: List<String>): List<String> {
+        return input
+    }
+
+    val input = readInput("day03")
+    println(part1(input))
+    println(part2(input))
+}
